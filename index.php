07292a5d01e2607afab56dd08847f9ace001ed49
httpxen <opulenciaandrei23@gmail.com>
2026-06-04 19:10:30 +0800

    Updates and add DockerFile

--- index.php
+++ index.php
@@ -217,12 +217,12 @@
                     </a>
                 </div>
                 <div class="flex items-center space-x-3">
-                    <a href="login.php" class="w-full bg-blue-900 text-white px-4 py-2 rounded-full font-semibold hover:bg-blue-800 dark:bg-gray-700 dark:hover:bg-gray-600 transition duration-300">LOGIN</a>
+                    <!--<a href="login.php" class="w-full bg-blue-900 text-white px-4 py-2 rounded-full font-semibold hover:bg-blue-800 dark:bg-gray-700 dark:hover:bg-gray-600 transition duration-300">LOGIN</a>
                     <button id="theme-toggle" class="bg-gray-200 dark:bg-gray-700 p-2 rounded-full focus:outline-none">
                         <svg id="theme-icon" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>
                         </svg>
-                    </button>
+                    </button> -->
                 </div>
             </div>
         </div>
